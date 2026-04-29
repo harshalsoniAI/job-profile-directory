f6dd60338cb2b7aa685541fc5ff518abaa227562
Harshal Soni <harshalsoniai@gmail.com>
2026-04-29 15:21:22 -0400

    Cleanup: Final import check in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Home from './pages/Home';
 import Search from './pages/Search';
 import JobDetail from './pages/JobDetail';
 import Directory from './pages/Directory';
-import { Navigate } from "react-router-dom";
+
 import './index.css';
 
 function AppContent() {
