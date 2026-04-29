deca29767f9a068a78b220410e2df0d03724f79c
Harshal Soni <harshalsoniai@gmail.com>
2026-04-29 16:18:02 -0400

    Fix: Restored vital API route imports

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const jobProfileRoutes = require("../backend/routes/jobProfiles");
+const jobProfileRoutes = require("./routes/jobProfiles");
 
 const app = express();
 
